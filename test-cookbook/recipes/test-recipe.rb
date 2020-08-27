df6a48985b38ddd77dd7bc4052aac6112bb0d0a0
root <root@ip-172-31-45-217.ap-south-1.compute.internal>
2020-08-27 14:47:04 +0000

    MyCookbooks

---
+++ test-cookbook/recipes/test-recipe.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook:: test-cookbook
+# Recipe:: test-recipe
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+
+file '/file1' do
+  content "Hello World, Good Morning !!"
+  action :create
+end
+
+#execute "run a script" do
+#  command <<-EOH
+#  mkdir /dungudir
+#  touch /dungufile
+#  EOH
+#end
+
+user "raj" do
+  action :create
+end
+
+group "DevOps" do
+  action :create
+  members 'raj'
+  append true
+end
+
+file '/testfile'
+user 'rahu'
